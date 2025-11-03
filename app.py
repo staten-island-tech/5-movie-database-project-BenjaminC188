be4621f50f985d5c7a4f0e2a170b837b12e7b7cd
BenjaminC188 <benjaminc188@nycstudents.net>
2025-11-03 06:25:03 -0800

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,43 @@ data = json.load(movies)
     print(item["title"])"""
 
 year = 0
-while data:
-    if year == input("select a year to get all movies made after that year."):
-        year > data["year"]
-        print(data["title"])
+movies = []
+while data["year"]:
+    year == input("Select a year to get all movies after that year")    
+    if data["year"] > year:
+        movies.append (data["title"])
+    print(movies)
+
+
+
+movies = []
+while data["year"]:
+    years = (x, y)
+    years == (input(years)) 
+    if data["year"] > x and data["year"] < y:
+        movies.append (data["title"])
+    print(movies)
+
+year = 0
+movies = []
+while data["year"]:
+    year == input("Select a year")
+if data["year"] == year:
+    movies.append(data["title"])
+    print(movies)
+
+def search():
+
+
+
+
+def genre():
+    genre = input("Pick a genre")
+    movies = []
+    while data["genre"]:
+        if data["genre"] == genre:
+            movies.append (data["title"])
+            print(movies)
+
+
+    
