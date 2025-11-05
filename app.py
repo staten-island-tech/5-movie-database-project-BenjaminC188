512d6b99b4861ebab0eb068a3e2e54880354189a
BenjaminC188 <benjaminc188@nycstudents.net>
2025-11-05 06:26:15 -0800

    Update app.py

--- app.py
+++ app.py
@@ -13,58 +13,8 @@ for movies in data:
         print(movies["title"])
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-"""
-movies = []
-while data["year"]:
-    years = (x, y)
-    years == (input(years)) 
-    if data["year"] > x and data["year"] < y:
-        movies.append (data["title"])
-    print(movies)
-
-year = 0
-movies = []
-while data["year"]:
-    year == input("Select a year")
-if data["year"] == year:
-    movies.append(data["title"])
-    print(movies)
-
-def search():
-
-
-
-
-def genre():
-    genre = input("Pick a genre")
-    movies = []
-    while data["genre"]:
-        if data["genre"] == genre:
-            movies.append (data["title"])
-            print(movies)
-"""
-
-    
+year = int(input("Pick a year to get movies made after that year."))
+yeartwo = int(input("Pick a year to get movies made before that year."))
+for movies in data:
+    if movies["year"] > year and movies[""]
+        print(movies["title"])
