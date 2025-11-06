dc32ccd2562f09fbb43eb5146ca3184ba3544062
BenjaminC188 <benjaminc188@nycstudents.net>
2025-11-06 06:22:18 -0800

    Update app.py

--- app.py
+++ app.py
@@ -7,14 +7,30 @@ data = json.load(movies)
 for index, item in enumerate(data):
     print(item["title"])
 
-year = int(input("Pick a year to get movies made after that year."))
+"""year = int(input("Pick a year to get movies made after that year."))
 for movies in data:
     if movies["year"] > year:
-        print(movies["title"])
+        print(movies["title"])"""
 
 
-year = int(input("Pick a year to get movies made after that year."))
-yeartwo = int(input("Pick a year to get movies made before that year."))
+"""year = int(input("pick a year to get all movies made after that year"))
+yeartwo = int(input("pick a year to get all movies made before that year"))
 for movies in data:
-    if movies["year"] > year and movies[""]
-        print(movies["title"])
+    if movies["year"] > year and movies["year"] < yeartwo:
+        print(movies["title"])"""
+
+"""year = int(input("pick a year to get all movies made during that year"))
+for movies in data:
+    if movies["year"] == year:
+        print(movies["title"])"""
+
+
+
+def search():
+    for movies in data:
+        search = input("Name of the movie:")
+        if search == movies["title"]:
+            return all
+
+
+
